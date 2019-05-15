e18bfb6e2122d13a1b599e6b5342977d31ee221d
root <root@ip-172-31-24-0.eu-central-1.compute.internal>
2019-05-15 19:37:40 +0000

    S3 update

--- roles/s3encryption/tasks/s3_encrypt.py
+++ roles/s3encryption/tasks/s3_encrypt.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
-
 import boto3
 import sys
 from botocore.exceptions import ClientError
 
 s3 = boto3.client('s3')
-
 response = s3.list_buckets()
 
 for bucket in response['Buckets']:
+
   try:
-    enc = s3.get_bucket_encryption(Bucket=bucket['Name'])
-    rules = enc['ServerSideEncryptionConfiguration']['Rules']
-    print('Bucket: %s - Status: Encrypted - Encryption: %s' % (bucket['Name'], rules))
+    getEncryption = s3.get_bucket_encryption(Bucket=bucket['Name'])
+    encryptionType = getEncryption['ServerSideEncryptionConfiguration']['Rules'][0]['ApplyServerSideEncryptionByDefault']['SSEAlgorithm']
+    print('Bucket: %s - Status: Encrypted - %s' % (bucket['Name'], encryptionType))
+
   except ClientError as e:
-    if any(bucket['Name'] in s for s in sys.argv):
-      print('Bucket: %s is in Exception List' % (bucket['Name']))
+    if any(bucket['Name'] in exceptionList for exceptionList in sys.argv):
+      print('Bucket: %s defined in Exception List' % (bucket['Name']))
     elif e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
       print('Bucket: %s - Status: No Server-Side Encryption' % (bucket['Name']))
     else:
