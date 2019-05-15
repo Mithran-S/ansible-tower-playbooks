558e70fd97119f8ca0629e6e1719266a4c2406ca
root <root@ip-172-31-24-0.eu-central-1.compute.internal>
2019-05-15 18:37:04 +0000

    s3 change

--- roles/s3encryption/tasks/s3_encrypt.py
+++ roles/s3encryption/tasks/s3_encrypt.py
@@ -4,10 +4,8 @@ import boto3
 import sys
 from botocore.exceptions import ClientError
 
-exc_bucket = sys.argv[1]
 s3 = boto3.client('s3')
 
-print exc_bucket
 response = s3.list_buckets()
 
 for bucket in response['Buckets']:
@@ -16,7 +14,7 @@ for bucket in response['Buckets']:
     rules = enc['ServerSideEncryptionConfiguration']['Rules']
     print('Bucket: %s - Status: Encrypted - Encryption: %s' % (bucket['Name'], rules))
   except ClientError as e:
-    if (bucket['Name']  == exc_bucket):
+    if any(bucket['Name'] in s for s in sys.argv):
       print('Bucket: %s is in Exception List' % (bucket['Name']))
     elif e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
       print('Bucket: %s - Status: No Server-Side Encryption' % (bucket['Name']))
